f09f402631a71a0353b597f055e7db0c85f5062a
Adam Charane <adamcharane@gmail.com>
2018-05-13 01:29:59 +0100

    Collecting the Pick Up Objects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -23,4 +23,9 @@ public class PlayerController : MonoBehaviour {
 		rb.AddForce (movement * speed);
 	}
 
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive(false);
+		}
+	}
 }
